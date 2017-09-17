9720c89d77bc68a570b912eda82946849d81e880
Peter Ebden <peter.ebden@gmail.com>
2017-09-17 19:37:55 +0100

    sort of kind of works a teeny bit more

--- js/webpack/main.js
+++ js/webpack/main.js
@@ -1,9 +1,11 @@
 const process = require('process');
 const webpack = require('webpack');
-const WebPackConfig = require(process.env.TOOLS_CONFIG);
 
 const buildBundle = function() {
-  const compiler = webpack(WebPackConfig());
+  // TODO(peter): This is a horrific hack to get around webpack overriding require().
+  //              There is bound to be a nicer way once I have internet to look it up...
+  const WebPackConfig = eval("require(process.env.TOOLS_CONFIG);");
+  const compiler = webpack(WebPackConfig(webpack));
   compiler.run(function(err, stats) {
     if (stats.compilation.errors.length > 0) {
       throw Error(stats.compilation.errors.join('\n'));
