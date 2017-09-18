ea664804e92f3f6a694119185c8339707a5f283f
Peter Ebden <peter.ebden@gmail.com>
2017-09-18 20:24:05 +0100

    adding babel

--- js/rollup/node.config.js
+++ js/rollup/node.config.js
@@ -3,13 +3,17 @@ const process = require('process');
 
 import resolve from 'rollup-plugin-node-resolve';
 import json from 'rollup-plugin-json';
+import babel from 'rollup-plugin-babel';
 
 export default {
     input: process.env.SRCS_JS,
     external: [],
     plugins: [
 	resolve(),
-	json()
+	json(),
+	babel({
+	    exclude: [ 'third_party/** '],
+	}),
     ],
     output: {
 	format: 'cjs',
