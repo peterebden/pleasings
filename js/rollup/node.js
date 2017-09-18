0057b4da4ac51a348b7a2611852b54620021e783
Peter Ebden <peter.ebden@gmail.com>
2017-09-18 19:43:18 +0100

    maybe progress?

--- js/rollup/node.js
+++ js/rollup/node.js
@@ -1,14 +1,10 @@
 const rollup = require('rollup');
-import 'config';
+require('./node.config');
+
 
 async function build() {
   // create a bundle
   const bundle = await rollup.rollup(config);
-
-  console.log(bundle.imports); // an array of external dependencies
-  console.log(bundle.exports); // an array of names exported by the entry point
-  console.log(bundle.modules); // an array of module objects
-
   // or write the bundle to disk
   await bundle.write(config.output);
 }
