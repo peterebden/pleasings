8153b7d8655a76ebbd2e744d06682594732524cf
Peter Ebden <peter.ebden@gmail.com>
2017-09-18 18:33:28 +0100

    let's try rollup

---
+++ js/rollup/node.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const process = require('process');
+const rollup = require('rollup');
+
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+// see below for details on the options
+const inputOptions = {
+    input: process.env.SRCS_JS,
+    external: [],
+    plugins: [
+	resolve(),
+	commonjs()
+    ]
+};
+const outputOptions = {
+    format: 'iife',
+    name: path.parse(process.env.OUTS_JS).name,
+};
+
+async function build() {
+  // create a bundle
+  const bundle = await rollup.rollup(inputOptions);
+
+  console.log(bundle.imports); // an array of external dependencies
+  console.log(bundle.exports); // an array of names exported by the entry point
+  console.log(bundle.modules); // an array of module objects
+
+  // or write the bundle to disk
+  await bundle.write(outputOptions);
+}
+
+build();
