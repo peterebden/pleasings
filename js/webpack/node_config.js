60acb69bc6cd6515ed5e5d103dc48ba9bd339ef0
Peter Ebden <peter.ebden@gmail.com>
2017-09-18 16:00:16 +0100

    more reworking

---
+++ js/webpack/node_config.js
@@ -0,0 +1,43 @@
+// This is the config for building Node binaries.
+// It's distinct from the config that we use for building JS for the web.
+const path = require('path');
+const process = require('process');
+const webpack = require('webpack');
+
+// Map plz's standard build config names to something meaningful to webpack.
+const buildConfig = process.env.BUILD_CONFIG;
+const nodeEnv = buildConfig === 'opt' ? 'production' : 'development';
+
+module.exports = {
+    entry: process.env.SRCS_JS.split(' ').map(src => './' + src),
+    target: 'node',
+    output: {
+	path: process.env.TMP_DIR,
+        filename: path.basename(process.env.OUTS_JS),
+    },
+    module: {
+	rules: [{
+	    test: /\.(js|jsx)$/,
+	    loader: 'babel-loader',
+	    query: {
+		presets: [
+		    'es2015',
+		],
+		plugins: []
+	    },
+	}]
+    },
+    resolve: {
+	modules: process.env.NODE_PATH.split(':'),
+    },
+    resolveLoader: {
+	modules: process.env.NODE_PATH.split(':'),
+    },
+    plugins: [
+	new webpack.DefinePlugin({
+	    'process.env': {
+		NODE_ENV: JSON.stringify(nodeEnv)
+	    }
+	})
+    ],
+};
--- js/webpack/node_main.js
+++ js/webpack/node_main.js
@@ -1,9 +1,9 @@
 const process = require('process');
 const webpack = require('webpack');
-const WebPackConfig = require('./config.js');
+const WebPackConfig = require('./node_config.js');
 
 const buildBundle = function() {
-  const compiler = webpack(WebPackConfig);
+  const compiler = webpack(WebPackConfig());
   compiler.run(function(err, stats) {
     if (stats.compilation.errors.length > 0) {
       throw Error(stats.compilation.errors.join('\n'));
